7ed105b9d41a20143d939d9efaa8eb9aeb3bc424
Abhishek Kumar <ckabhishek96.srm@gmail.com>
2023-05-27 16:34:04 +0530

    installed sass library
    installed the sass library and reset all the default setting for scss

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./components/App";
 import store from "./redux/store/store";
+import "./GlobalStyle.scss";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
