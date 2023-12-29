7c3ce56e2e8a5a3ad26defbaeeeec6f818dc424d
Abhishek Kumar <ckabhishek96.srm@gmail.com>
2023-12-29 18:11:02 +0530

    refactor: Refactore HttpClient and middleware
    Description:
    - Refactor HttpClient and middleware for seperation of concern and more readability

--- frontend/src/core/api/HttpClient.ts
+++ frontend/src/core/api/HttpClient.ts
@@ -3,94 +3,99 @@ import { generateUrlString, isTokenExpired } from "./HttpUtils";
 import { SecureStorage } from "../../redux/store/store";
 import { refreshToken as refreshTokenUrl } from "../../api/routes";
 
-export const httpService = (
-  { method, path, routeParameters = {}, body = {} }: RequestOptions,
-  isProtected = false
-) => {
-  const url = generateUrlString(path, routeParameters);
+// export const httpService = (
+//   { method, path, routeParameters = {}, body = {} }: RequestOptions,
+//   isProtected = false
+// ) => {
+//   const url = generateUrlString(path, routeParameters);
 
-  const headers: Headers = new Headers();
-  if (body instanceof FormData) {
-    headers.append("Content-Type", "multipart/form-data");
-  } else {
-    headers.append("Content-Type", "application/json");
-  }
+//   const headers: Headers = new Headers();
+//   if (body instanceof FormData) {
+//     headers.append("Content-Type", "multipart/form-data");
+//   } else {
+//     headers.append("Content-Type", "application/json");
+//   }
 
-  if (isProtected) {
-    const accessToken: String | null = SecureStorage.getAccessToken();
-    headers.append("Authorization", `Bearer ${accessToken}`);
-  }
+//   if (isProtected) {
+//     const accessToken: String | null = SecureStorage.getAccessToken();
+//     headers.append("Authorization", `Bearer ${accessToken}`);
+//   }
 
-  // const headers: Headers = {
-  //   "Content-Type":
-  //     body instanceof FormData ? "multipart/form-data" : "application/json",
-  //     // "Authorization": isProtected ? SecureStorage.getAccessToken() : ''
-  // };
+//   // const headers: Headers = {
+//   //   "Content-Type":
+//   //     body instanceof FormData ? "multipart/form-data" : "application/json",
+//   //     // "Authorization": isProtected ? SecureStorage.getAccessToken() : ''
+//   // };
 
-  const config: RequestInit = {
-    method,
-    headers,
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: "same-origin", // include, *same-origin, omit
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer",
-    body:
-      method === "POST" || method === "PUT" || method === "PATCH"
-        ? body instanceof FormData
-          ? body
-          : JSON.stringify(body)
-        : undefined,
-  };
+//   const config: RequestInit = {
+//     method,
+//     headers,
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     // credentials: "same-origin", // include, *same-origin, omit
+//     redirect: "follow", // manual, *follow, error
+//     referrerPolicy: "no-referrer",
+//     body:
+//       method === "POST" || method === "PUT" || method === "PATCH"
+//         ? body instanceof FormData
+//           ? body
+//           : JSON.stringify(body)
+//         : undefined,
+//   };
 
-  return requestInterceptor(url, config, isProtected);
-};
+//   return requestInterceptor(url, config, isProtected);
+// };
 
-const requestInterceptor = async (
-  url: URL | "",
-  options: RequestInit,
-  isProtected: boolean
-) => {
-  const accessToken: String | null = SecureStorage.getAccessToken();
-  if (accessToken !== null && isProtected) {
-    const newHeaders = new Headers(options.headers);
-    newHeaders.delete("Authorization");
-    newHeaders.append("Authorization", `Bearer ${accessToken}`);
-    options.headers = newHeaders;
-  }
+// const requestInterceptor = async (
+//   url: URL | "",
+//   options: RequestInit,
+//   isProtected: boolean
+// ) => {
+//   const accessToken: String | null = SecureStorage.getAccessToken();
+//   if (accessToken !== null && isProtected) {
+//     const newHeaders = new Headers(options.headers);
+//     newHeaders.delete("Authorization");
+//     newHeaders.append("Authorization", `Bearer ${accessToken}`);
+//     options.headers = newHeaders;
+//   }
 
-  if (isProtected && accessToken !== null && isTokenExpired(accessToken)) {
-    const refreshToken: String | null = SecureStorage.getRefreshToken();
-    if (refreshToken) {
-      try {
-        const refreshUrl = generateUrlString(refreshTokenUrl, {});
-        const response = await fetch(refreshUrl, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${refreshToken}`,
-          },
-        });
-        const result = await response.json();
-        if (response.ok && response.status === 200) {
-          SecureStorage.setRefreshToken(result.result.refreshToken);
-          SecureStorage.setAccessToken(result.result.accessToken);
+//   if (isProtected && accessToken !== null && isTokenExpired(accessToken)) {
+//     const refreshToken: String | null = SecureStorage.getRefreshToken();
+//     if (refreshToken) {
+//       try {
+//         const refreshUrl = generateUrlString(refreshTokenUrl, {});
+//         const response = await fetch(refreshUrl, {
+//           method: "POST",
+//           headers: {
+//             Authorization: `Bearer ${refreshToken}`,
+//           },
+//         });
+//         const result = await response.json();
+//         if (response.ok && response.status === 200) {
+//           SecureStorage.setRefreshToken(result.result.refreshToken);
+//           SecureStorage.setAccessToken(result.result.accessToken);
 
-          const newHeaders = new Headers(options.headers);
-          newHeaders.delete("Authorization");
-          newHeaders.append("Authorization", `Bearer ${accessToken}`);
-          options.headers = newHeaders;
-        } else {
-          // TODO:: Handle the failuire and notify user about it.
-          throw new Error(`Request Failed with status ${response.status}`);
-        }
-      } catch (error) {
-        SecureStorage.removeToken();
-      }
-    }
-  }
+//           const newHeaders = new Headers(options.headers);
+//           newHeaders.delete("Authorization");
+//           newHeaders.append("Authorization", `Bearer ${accessToken}`);
+//           options.headers = newHeaders;
+//         } else {
+//           SecureStorage.removeToken();
+//           // TODO:: Handle the failuire and notify user about it.
+//           throw new Error(`Request Failed with status ${response.status}`);
+//         }
+//       } catch (error) {
+//         SecureStorage.removeToken();
+//       }
+//     }
+//   }
 
-  return fetch(url, options);
-};
+//   return fetch(url, options);
+// };
+
+// const httpPostOperation = (postFunction) => {
+
+// }
 
 // const fetchApi = (url: any, method: HttpMethod, body: any) => {
 //   const config: RequestInit = {
@@ -114,3 +119,90 @@ const requestInterceptor = async (
 
 //   return fetch(url, config);
 // };
+
+/*
+ * New Improve logic to perform http request
+ */
+// TODO:: apply dependency inversion for secure storage and refresh url
+
+const createHeaders = (body: FormData | Record<string, any>) => {
+  const headers: Headers = new Headers();
+  if (body instanceof FormData) {
+    headers.append("Content-Type", "multipart/form-data");
+  } else {
+    headers.append("Content-Type", "application/json");
+  }
+  return headers;
+};
+
+const appendAuthorizationHeader = (
+  options: RequestInit,
+  accessToken: string
+) => {
+  const newHeaders = new Headers(options.headers);
+  newHeaders.delete("Authorization");
+  newHeaders.append("Authorization", `Bearer ${accessToken}`);
+  options.headers = newHeaders;
+};
+
+const refreshTokenIfNeeded = async (options: RequestInit) => {
+  const accessToken = SecureStorage.getAccessToken();
+  if (!accessToken || !isTokenExpired(accessToken)) return;
+
+  const refreshToken = SecureStorage.getRefreshToken();
+  if (!refreshToken) {
+    SecureStorage.removeToken();
+    return;
+  }
+
+  try {
+    const response = await fetch(generateUrlString(refreshTokenUrl, {}), {
+      method: "POST",
+      headers: { Authorization: `Bearer ${refreshToken}` },
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      SecureStorage.setRefreshToken(result.result.refreshToken);
+      SecureStorage.setAccessToken(result.result.accessToken);
+      appendAuthorizationHeader(options, result.result.accessToken);
+    } else {
+      SecureStorage.removeToken();
+      throw new Error(`Request Failed with status ${response.status}`);
+    }
+  } catch (error) {
+    SecureStorage.removeToken();
+    throw new Error("Token Refresh Failed");
+  }
+};
+
+export const httpService = async (
+  { method, path, routeParameters = {}, body = {} }: RequestOptions,
+  isProtected = false
+) => {
+  const url = generateUrlString(path, routeParameters);
+  const headers = createHeaders(body);
+
+  const options: RequestInit = {
+    method,
+    headers,
+    mode: "cors",
+    cache: "no-cache",
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body:
+      method === "POST" || method === "PUT" || method === "PATCH"
+        ? body instanceof FormData
+          ? body
+          : JSON.stringify(body)
+        : undefined,
+  };
+
+  if (isProtected) {
+    const accessToken = SecureStorage.getAccessToken();
+    if (accessToken) appendAuthorizationHeader(options, accessToken);
+    await refreshTokenIfNeeded(options);
+  }
+
+  return fetch(url, options);
+};
--- frontend/src/core/middleware/http.middleware.ts
+++ frontend/src/core/middleware/http.middleware.ts
@@ -2,59 +2,119 @@ import { Middleware } from "@reduxjs/toolkit";
 import { getApiRegistry } from "../api/apiRegistry";
 import { HTTP_ERROR, HTTP_REQUEST } from "../actionType/coreActionType";
 
+const handleSuccess = (responseData: any, onSuccess: any, next: any) => {
+  const OnSuccessHandler = onSuccess(responseData);
+  if (Array.isArray(OnSuccessHandler)) {
+    OnSuccessHandler.forEach((act: any) => {
+      next(act);
+    });
+  }
+};
+
+const handleError = (response: Response, onError: any, next: any) => {
+  const errorData = {
+    errorCode: response.status,
+    errorMessage: Object.keys(response).length ? response : "",
+  };
+  const OnErrorHandler = onError(errorData);
+  if (Array.isArray(OnErrorHandler)) {
+    OnErrorHandler.forEach((err: any) => {
+      next(err);
+    });
+  }
+  next({ type: HTTP_ERROR, payload: { isError: true, ...errorData } });
+  setTimeout(() => {
+    next({ type: HTTP_ERROR, payload: {} });
+  }, 3000);
+};
+
+const executeApiCall = async (action: any, apiHandler: any, next: any) => {
+  next({ type: HTTP_REQUEST, payload: { action, inProgress: true } });
+
+  try {
+    const response = await apiHandler.fetchCall(action);
+    const responseData = apiHandler.postOp(await response.json());
+
+    if (response.ok && response.status === 200) {
+      handleSuccess(responseData, apiHandler.onSuccess, next);
+    } else {
+      handleError(response, apiHandler.onError, next);
+    }
+  } catch (error) {
+    console.error(error);
+    next({ type: HTTP_ERROR, payload: {} });
+  } finally {
+    next({ type: HTTP_REQUEST, payload: { action, inProgress: false } });
+  }
+};
+
 export const myMiddleware: Middleware =
   (storeAPI) => (next) => async (action) => {
     const apiHandler = getApiRegistry(action.type);
     if (apiHandler === null) {
-      // Not an api call and moving to next middleware
       return next(action);
     }
 
-    const { fetchCall, onSuccess, onError, postOp } = apiHandler;
-    try {
-      next(action);
-      // Set http request status to true
-      next({ type: HTTP_REQUEST, payload: { action, inProgress: true } });
-
-      const response = await fetchCall(action);
-      const responseData = postOp(await response.json());
-
-      // Execute success handler
-      if (response.ok && response.status === 200) {
-        const OnSuccessHandler = onSuccess(responseData);
-        if (Array.isArray(OnSuccessHandler)) {
-          OnSuccessHandler.forEach((act) => {
-            next(act);
-          });
-
-          // Set http request status to false
-          next({ type: HTTP_REQUEST, payload: { action, inProgress: false } });
-        }
-      } else {
-        // Execute error handler
-        const errorData = {
-          errorCode: response.status,
-          errorMessage: Object.keys(responseData).length ? responseData : "",
-        };
-        const OnErrorHandler = onError(errorData);
-        if (Array.isArray(OnErrorHandler)) {
-          OnErrorHandler.forEach((err) => {
-            next(err);
-          });
-        }
-
-        // Set http request status to false
-        next({ type: HTTP_REQUEST, payload: { action, inProgress: false } });
-
-        // set httpError
-        next({ type: HTTP_ERROR, payload: { isError: true, ...errorData } });
-        setTimeout(() => {
-          next({ type: HTTP_ERROR, payload: {} });
-        }, 3000);
-      }
-    } catch (error) {
-      console.error("error");
-      return next(action);
-    }
-    return;
+    await executeApiCall(action, apiHandler, next);
   };
+
+/*
+ * Old code
+ */
+
+// export const myMiddleware: Middleware =
+//   (storeAPI) => (next) => async (action) => {
+//     const apiHandler = getApiRegistry(action.type);
+//     if (apiHandler === null) {
+//       // Not an api call and moving to next middleware
+//       return next(action);
+//     }
+
+//     const { fetchCall, onSuccess, onError, postOp } = apiHandler;
+//     try {
+//       next(action);
+//       // Set http request status to true
+//       next({ type: HTTP_REQUEST, payload: { action, inProgress: true } });
+
+//       const response = await fetchCall(action);
+//       const responseData = postOp(await response.json());
+
+//       // Execute success handler
+//       if (response.ok && response.status === 200) {
+//         const OnSuccessHandler = onSuccess(responseData);
+//         if (Array.isArray(OnSuccessHandler)) {
+//           OnSuccessHandler.forEach((act) => {
+//             next(act);
+//           });
+
+//           // Set http request status to false
+//           next({ type: HTTP_REQUEST, payload: { action, inProgress: false } });
+//         }
+//       } else {
+//         // Execute error handler
+//         const errorData = {
+//           errorCode: response.status,
+//           errorMessage: Object.keys(responseData).length ? responseData : "",
+//         };
+//         const OnErrorHandler = onError(errorData);
+//         if (Array.isArray(OnErrorHandler)) {
+//           OnErrorHandler.forEach((err) => {
+//             next(err);
+//           });
+//         }
+
+//         // Set http request status to false
+//         next({ type: HTTP_REQUEST, payload: { action, inProgress: false } });
+
+//         // set httpError
+//         next({ type: HTTP_ERROR, payload: { isError: true, ...errorData } });
+//         setTimeout(() => {
+//           next({ type: HTTP_ERROR, payload: {} });
+//         }, 3000);
+//       }
+//     } catch (error) {
+//       console.error("error");
+//       return next(action);
+//     }
+//     return;
+//   };
