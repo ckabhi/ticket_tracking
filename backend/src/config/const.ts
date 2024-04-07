c134c45a41653e907ff6bcf6d778142a94956a9b
Abhishek Kumar <ckabhishek96.srm@gmail.com>
2024-04-07 15:10:34 +0530

    add: Reduce the expiry time for access token
    Description:
    - Reduced the access token expiry time to 2 minutes

--- backend/src/config/const.ts
+++ backend/src/config/const.ts
@@ -1,7 +1,7 @@
 export const JWT_SECREATE: string = "gOPINSGFiYDtYdiUyn@udOVbsg8463#2$";
 export const SALT_ROUND: number = 10;
 export const REFRESH_TOKEN_VALIDITY = "2d";
-export const ACCESS_TOKEN_VALIDITY = "1h";
+export const ACCESS_TOKEN_VALIDITY = "2m"; // 1h
 export const AUTH_TOKEN_TYPE = {
   refresh: "refresh",
   access: "access",
